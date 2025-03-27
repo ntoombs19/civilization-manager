0845500bcb4a62fc83b3eba67df88877a8a345a6
Nathan Toombs <ntoombs19@gmail.com>
2025-03-27 14:31:05 -0500

    Add additional tailwind styles and fonts

--- tailwind.config.js
+++ tailwind.config.js
@@ -14,7 +14,26 @@ export default {
         extend: {
             fontFamily: {
                 sans: ['Figtree', ...defaultTheme.fontFamily.sans],
+                serif: ['Trajan Pro', ...defaultTheme.fontFamily.serif],
             },
+            animation: {
+                'ping': 'ping 2s ease-in-out infinite',
+                'twinkle': 'twinkle 8s infinite',
+            },
+            keyframes: {
+                ping: {
+                    '0%': { transform: 'scale(1)', opacity: '0.8' },
+                    '70%, 100%': { transform: 'scale(1.5)', opacity: '0' },
+                },
+                twinkle: {
+                    '0%': { opacity: '0' },
+                    '50%': { opacity: '1' },
+                    '100%': { opacity: '0' },
+                },
+            },
+            shadow: {
+                'text': '0 0 8px rgba(255, 215, 0, 0.5)',
+            }
         },
     },
 
