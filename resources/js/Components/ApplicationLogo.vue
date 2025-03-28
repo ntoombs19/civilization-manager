b55ecfbbed8ebb6e2072e9ee6b5dafb2ebf052d2
Nathan Toombs <ntoombs19@gmail.com>
2025-03-27 21:00:58 -0500

    Fix ApplicationLogo

--- resources/js/Components/ApplicationLogo.vue
+++ resources/js/Components/ApplicationLogo.vue
@@ -1,5 +1,5 @@
 <template>
     <div class="mr-3 flex items-center justify-center">
-        <div class="flex items-center justify-center w-10 h-10 rounded-full bg-gradient-to-br from-amber-400 to-amber-600 text-stone-900 font-extrabold text-lg shadow-lg shadow-amber-600/40"><span class="mt-1">VI</span></div>
+        <div class="flex items-center justify-center w-10 h-10 rounded-full bg-gradient-to-br from-amber-400 to-amber-600 text-stone-900 font-extrabold text-lg shadow-lg shadow-amber-600/40"><span class="mt-1">V</span></div>
     </div>
 </template>
